616384359fa4c5e9adef7787513af5f2210d7bff
lopof <thomas.baumann90@gmx.ch>
2019-04-02 15:30:24 +0200

    lang detect

---
+++ camel-example-twitter-websocket/src/main/java/org/apache/camel/example/websocket/LangDetect.java
@@ -0,0 +1,18 @@
+package org.apache.camel.example.websocket;
+
+import com.cybozu.labs.langdetect.Detector;
+import com.cybozu.labs.langdetect.DetectorFactory;
+import com.cybozu.labs.langdetect.LangDetectException;
+
+public class LangDetect {
+
+    public void init(String profileDirectory) throws LangDetectException {
+        DetectorFactory.loadProfile(profileDirectory);
+    }
+
+    public String detect(String text) throws LangDetectException {
+        Detector detector = DetectorFactory.create();
+        detector.append(text);
+        return detector.detect();
+    }
+}
---
+++ camel-example-twitter-websocket/src/test/java/org/apache/camel/example/websocket/CamelTwitterWebSocketMainTest.java
@@ -0,0 +1,12 @@
+package org.apache.camel.example.websocket;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CamelTwitterWebSocketMainTest {
+
+    @Test
+    void main() {
+    }
+}
